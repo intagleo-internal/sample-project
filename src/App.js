5b235af053103f99bc7942435c8d9cf8aa2cbabf
NINEALPHA\ehsan.ali <Ehsan.Ali@intagleo.com>
2019-06-17 18:22:17 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 import LoginScreen from "./screens/LoginScreen";
